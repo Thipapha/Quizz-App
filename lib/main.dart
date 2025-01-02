6fedb0049b351f3c99a7c54b474faf40757860a6
thipaphajindarak.preaw <thipaphajindarak.preaw@gmail.com>
2025-01-02 18:23:29 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/start_screen.dart';
+import 'package:myapp/questions_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.lightBlue, Colors.yellow],
+        ),
+      ),
+      child: const QuestionsScreen(),
+    ),
+  ),),);
+}
---
+++ lib/model/quizz_model.dart
@@ -0,0 +1,6 @@
+class QuizzModel {
+  String question;
+  List<String> answers;
+
+  QuizzModel(this.question,this.answers);
+}
